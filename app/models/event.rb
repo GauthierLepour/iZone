b965c22f71a2e6f5ae09ab371d443852e04ec932
Benjamin Moreau <benjamin.moreau49@gmail.com>
2024-03-05 12:01:56 +0100

    added validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,18 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :rides
+
+  validates :name, :start_time, :end_time, presence: true
+  validates :name, length: { maximum: 20 }
+  validate :end_date_after_start_date, :start_date_after_today
+
+  private
+
+  def end_date_after_start_date
+    errors.add(:end_time, "must be after the start date") if end_time < start_time
+  end
+
+  def start_date_after_today
+    errors.add(:start_time, "cannot be in the past") if start_time < Time.now
+  end
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,6 @@
 class Ride < ApplicationRecord
   belongs_to :event
   belongs_to :car
+  has_many :passenger_requests
+  validates :departure_place, :arrival_place, :departure_time, presence: true
 end
